340d6be543770a5517c3447e2bb04f6650f17d81
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-07-31 17:46:41 +0530

    first 4 problems in the 100 basic programs

---
+++ lev1.py
@@ -0,0 +1,25 @@
+'''
+# Write a program to print "Hello, World!" on the screen.
+print("Hello, World!")
+
+# Write a program to read two numbers and print their sum.
+a=2
+b=7
+print(a+b)
+
+# Write a program to read two numbers and print their sum, difference, product and quotient.
+a=8
+b=3
+sum = a+b
+difference = a-b
+product = a*b
+quotient = a/b
+print(sum, difference, product, quotient)
+
+# Write a program to read the radius of a circle and print its area and circumference.
+import math
+radius = 6
+area = math.pi * radius**2
+circumference = 2 * math.pi * radius
+print(area, circumference)
+'''

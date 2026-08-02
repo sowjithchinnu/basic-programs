5aea4c5daabd3671817853bcd0f51c3772c50a51
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-03 01:09:23 +0530

    done with the 12th problem in the 100 basic programs

---
+++ lev2.py
@@ -0,0 +1,17 @@
+'''
+# Write a program to read a number and check whether it is even or odd.
+number = int(input("enter a number: "))
+if number % 2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+    
+# Write a program to read a number and check whether it is positive, negative or zero.
+number = int(input("enter a number: "))
+if number > 0:
+    print("number is positive")
+elif number < 0:
+    print("number is negative")
+else:
+    print("the number is zero")
+'''

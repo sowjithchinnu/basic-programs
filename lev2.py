4c222cf0795aae89094776e0e7b777f6cef16446
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-04 01:22:47 +0530

    done with the 15th problem in the 100 basic programs

--- lev2.py
+++ lev2.py
@@ -14,4 +14,34 @@ elif number < 0:
     print("number is negative")
 else:
     print("the number is zero")
+
+# Write a program to read three numbers and find the largest among them.
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+num3 = int(input("enter a number: "))
+if num1 >= num2 and num1 >= num3:
+    print(f"{num1} is the largest number")
+elif num2 >= num1 and num2 >= num3:
+    print(f"{num2} is the largest number")
+else:
+    print(f"{num3} is the largest number")
+
+# Write a program to read three numbers and find the smallest among them.
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+num3 = int(input("enter a number: "))
+if num1 <= num2 and num1 <= num3:
+    print(f"{num1} is the smallest number")
+elif num2 <= num1 and num2 <= num3:
+    print(f"{num2} is the smallest number")
+else:
+    print(f"{num3} is the smallest number")
+    
+# Write a program to read a year and check whether it is a leap year or not.
+year = int(input("enter a year: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("it is a leap year")
+else:
+    print("it is not a leap year")
 '''
+

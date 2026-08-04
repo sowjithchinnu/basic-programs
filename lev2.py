adf495a900df7afea045358515e3d0e89b63613e
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-05 00:40:23 +0530

    completed first 20 problems in the 100 basic programs

--- lev2.py
+++ lev2.py
@@ -54,4 +54,49 @@ if len(char) == 1 and lower.isalpha():
         print("it is a consonant")
 else:
     print("enter a single valid alphabet")
+
+# Write a program to read a character and check whether it is an alphabet, digit or special symbol.
+char = input("enter a character: ")
+lower = char.lower()
+if len(char) == 1:
+    if lower.isalpha():
+        print("it is an alphabet")
+    elif char.isdigit():
+        print("it is a digit")
+    else:
+        print("it is a special symbol")
+else:
+    print("enter a single valid character")
+
+# Write a program to read the marks of a student and print the grade (A/B/C/D/Fail).
+marks = int(input("Enter your marks: "))
+if 0 <= marks <= 100:
+    if marks > 90:
+        print("A grade")
+    elif marks > 80:
+        print("B grade")
+    elif marks > 70:
+        print("C grade")
+    elif marks > 60:
+        print("D grade")
+    elif marks >= 40:
+        print("E grade")
+    else:
+        print("Fail")
+else:
+    print("enter a valid score between 0 and 100")
+
+# Write a program to read a number and check whether it is divisible by both 3 and 5.
+num = int(input("enter a number: "))
+if num % 3 == 0 and num % 5 == 0:
+    print("the number is divisible by both 3 and 5")
+else:
+    print("the number is not divisible by both 3 and 5")
+
+# Write a program to read the age of a person and check whether they are eligible to vote.
+age = int(input("enter your age:"))
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
 '''

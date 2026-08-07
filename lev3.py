21002c62ab2ea32545fe85a4ed9c2c40829a83c9
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-08 02:16:14 +0530

    completed 23 problems in the 100 basic programs

--- lev3.py
+++ lev3.py
@@ -1,4 +1,18 @@
+'''
 # Write a program to display all the natural numbers from 1 to n. (n is user input)
 n = int(input("enter a number: "))
 for i in range(1, 1+n):
     print(i)
+
+# Write a program to display all natural numbers from 1 to n in reverse order.
+n = int(input("enter a number : "))
+while n>=1:
+    print(n)
+    n -= 1
+
+# Write a program to display all even numbers from 1 to n.
+n = int(input("enter a number: "))
+for i in range(1, 1+n):
+    if i % 2 == 0:
+        print(i)
+'''

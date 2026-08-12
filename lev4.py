5777ba3bc11aad5c1e9de46359fb4b4c6a237ef3
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-12 12:19:26 +0530

    completed 35 problems in the 100 basic programs

--- lev4.py
+++ lev4.py
@@ -1,5 +1,32 @@
-
+'''
 # Write a program to count the number of digits in a number n.
 n = int(input("enter a number: "))
-length= len(str(abs(n)))
-print(length)
+temp = abs(n)
+count = 0 if temp > 0 else 1
+while temp > 0:
+    temp //= 10
+    count += 1
+print(count)
+
+# Write a program to display all the digits of a number n (one per line).
+n = int(input("enter a number: "))
+temp = abs(n)
+digits = []
+if temp == 0:
+    digits.append(temp)
+while temp > 0:
+    last = temp % 10
+    digits.append(last)
+    temp //= 10
+for digits in reversed(digits):
+    print(digits)
+
+# Write a program to find the sum of all digits of a number n.
+n = int(input("enter a number: "))
+temp = abs(n)
+total_sum = 0
+while temp > 0:
+    total_sum += temp % 10
+    temp //= 10
+print(total_sum)
+'''

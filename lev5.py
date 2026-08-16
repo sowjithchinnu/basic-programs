ea3502baea8939d252cacde3f310eb3e53c16557
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-16 23:52:27 +0530

    completed 44 problems in the 100 basic programs

---
+++ lev5.py
@@ -0,0 +1,15 @@
+# Write a program to read a number and check whether it is prime or not.
+n = int(input("enter a number: "))
+if n <= 1:
+    print(f"{n} is not a prime number")
+else:
+    is_prime = True
+    
+    for i in range(2, int(n**(0.5)) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    if is_prime :
+        print(f"{n} is a prime number")
+    else:
+        print(f"{n} is not a prime number")

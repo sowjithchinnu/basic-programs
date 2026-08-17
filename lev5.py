f86f35ce4bbc28f12c62d40e769d1fd60f1615dc
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-08-18 01:16:46 +0530

    completed 43 problems in the 100 basic programs

--- lev5.py
+++ lev5.py
@@ -1,4 +1,5 @@
-# Write a program to read a number and check whether it is prime or not.
+
+'''# Write a program to read a number and check whether it is prime or not.
 n = int(input("enter a number: "))
 if n <= 1:
     print(f"{n} is not a prime number")
@@ -13,3 +14,18 @@ else:
         print(f"{n} is a prime number")
     else:
         print(f"{n} is not a prime number")
+
+# Write a program to display all prime numbers from 1 to n.
+n = int(input("enter a number: "))
+if n <= 1:
+    print("no prime numbers!")
+else:
+    for i in range(2, n+1):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i%j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(i)
+'''

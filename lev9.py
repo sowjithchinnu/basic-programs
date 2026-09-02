dded04d5325a9deb67673325a9e721e5c593bce3
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-09-03 00:52:52 +0530

    completed 96 problems in the 100 basic programs

--- lev9.py
+++ lev9.py
@@ -28,12 +28,42 @@ else:
 
 # Write a program to find the sum of digits of a number repeatedly until a single digit remains.
 n = int(input("enter a number: "))
-total = n
 while n >= 10:
     total = 0
     while n > 0:
         total += n % 10
         n //= 10
     n = total
-print(total)
+print(n)
+
+# Write a program to count the number of prime digits present in a number n.
+n = int(input("enter a number: "))
+prime_digits = {2, 3, 5, 7}
+count = 0
+for i in str(n):
+    if int(i) in prime_digits:
+        count += 1
+print(count)
+
+# Write a program to check whether a number is a palindrome and a prime at the same time.
+n = int(input("enter a number: "))
+num = n
+rev = 0
+while num > 0:
+    last = num % 10
+    rev = rev * 10 + last
+    num //= 10
+palin = (rev == n)
+if n < 2:
+    isPrime = False
+else:
+    isPrime = True
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            isPrime = False
+            break
+if isPrime and palin:
+    print(True)
+else:
+    print(False)
 '''

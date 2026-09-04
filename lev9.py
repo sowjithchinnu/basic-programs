88434cd206e2d8b4ce525644cf760e9a192c1219
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-09-04 14:32:52 +0530

    FEAT: completed 100 problems in the 100 basic programs

--- lev9.py
+++ lev9.py
@@ -82,11 +82,13 @@ print(f"the smallest and largest number that can be formed using the number {n} 
 # Write a program to convert a decimal number into its binary equivalent.
 n = int(input("enter a number: "))
 binary = ""
-while n > 0:
-    binary = str(n%2) + binary
-    n //= 2
-print(binary)
-'''
+if n == 0:
+    print("0")
+else:
+    while n > 0:
+        binary = str(n%2) + binary
+        n //= 2
+    print(binary)
 
 # Write a program to convert a binary number into its decimal equivalent.
 n = input("enter a binary number: ")
@@ -97,3 +99,69 @@ for digit in (reversed(n)):
         decimal += 2 ** power
     power += 1
 print(decimal)
+
+# Write a program to display a menu that lets the user repeatedly choose any of the above tasks until they choose to exit.
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def digitSum(n):
+    while n >= 10:
+        total = 0
+        while n > 0:
+            total += n % 10
+            n //= 10
+        n = total
+    return n
+    
+def palindromeAndPrime(n):
+    num = n
+    rev = 0
+    while num > 0:
+        last = num % 10
+        rev = rev * 10 + last
+        num //= 10
+    palin = (rev == n)
+    return palin and isPrime(n)
+
+def decimalToBinary(n):
+    binary = ""
+    if n == 0:
+        return "0"
+    else:
+        while n > 0:
+            binary = str(n%2) + binary
+            n //= 2
+        return binary
+
+while True:
+    print("\n1. check prime")
+    print("2. check the repeated sum of digits")
+    print("3. check if is both palindrome and prime")
+    print("4. convert decimal to binary")
+    print("5. Exit")
+    
+    choice = int(input("enter your choice: "))
+    
+    if choice == 1:
+        n = int(input("enter a number: "))
+        print(isPrime(n))
+    elif choice == 2:
+        n = int(input("enter a number: "))
+        print(digitSum(n))
+    elif choice == 3:
+        n = int(input("enter a number: "))
+        print(palindromeAndPrime(n))
+    elif choice == 4:
+        n = int(input("enter a number: "))
+        print(decimalToBinary(n))
+    elif choice == 5:
+        print("Thank you!")
+        break
+    else:
+        print("invalid choice")
+'''
